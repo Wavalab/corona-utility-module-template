cbf5e56f3b53026d1bb57094c9f8d4eb488facf6
John Chin-Jew <jchinjew@gmail.com>
2016-08-20 13:14:51 -0700

    init

---
+++ utility-module-template.lua
@@ -0,0 +1,38 @@
+--
+-- Utility Module
+--
+
+local M = {}
+-- include require statements here
+
+--
+-- Private variables
+--
+
+local str
+
+--
+-- Private functions
+--
+
+local privFn1 = function()
+  str = "Test"
+end
+
+local publicFn1 = function()
+  privFn1()
+  print( str + " 1" )
+end
+
+local publicFn2 = function()
+  privFn1()
+  print( str + " 2" )
+end
+
+--
+-- Expose module API
+--
+
+M.test1 = publicFn1
+M.test2 = publicFn2
+return M
